7415f3cf4d65ee6915bad0af34d4d3d85249b839
Sarah Baskin <sarahbaskin@brandeis.edu>
2024-10-05 20:21:15 -0400

    fixing the green line convergence issue

--- MBTA.py
+++ MBTA.py
@@ -1,7 +1,9 @@
 from TrainTypes import Subway
 from TrainTypes import Station
+from collections import defaultdict
 
 def main():
+
 
     Alewife = Station("Alewife", "Subway and Bus")
     Davis = Station("Davis", "Subway and Bus")
@@ -27,6 +29,25 @@ def main():
     Quincy_Center = Station("Quincy Center", "Subway and Bus")
     Quincy_Adams = Station("Quincy Adams", "Subway and Bus")
     Braintree = Station("Braintree", "Subway and Bus")
+    Arlington = Station("Arlington", "Subway and Bus")
+    Copley = Station("Copley", "Subway and Bus")
+    Hynes_Convention = Station("Hynes Convention Center", "Subway and Bus")
+    Kenmore = Station("Kenmore", "Subway and Bus")
+    Blandford = Station("Blandford Street", "Subway and Bus")
+    Saint_Mary = Station("Saint Mary's Street", "Subway and Bus")
+    Fenway = Station("Fenway", "Subway and Bus")
+    Prudential = Station("Prudential", "Subway and Bus")
+    Union_Square = Station("Union Square", "Subway and Bus")
+    Lechmere = Station("Lechmere", "Subway and Bus")
+    Science_Park = Station("Science Park/West End", "Subway and Bus")
+    North_Station = Station("North Station", "Subway and Bus")
+    Haymarket = Station("Haymarket", "Subway and Bus")
+    Tufts = Station("Medford/Tufts", "Subway and Bus")
+    Ball_Square = Station("Ball Square", "Subway and Bus")
+    Magoun_Square = Station("Magoun Square", "Subway")
+    Gilman_Square = Station("Gilman Square", "Subway and Bus")
+    East_Som = Station("East Somerville", "Subway")
+
 
     Davis_Red = Subway("Davis", "Red Line")
     Porter_Red = Subway("Porter", "Red Line")
@@ -50,20 +71,20 @@ def main():
     Quincy_Adams_Red = Subway("Quincy Adams", "Red Line")
     Braintree_Red = Subway("Braintree", "Red Line")
 
-    Park_Street_B = Subway("Park Street", "Green Line -- B")
-    Boylston_B = Subway("Boylston", "Green Line -- B")
-    Arlington_B = Subway("Arlington", "Green Line -- B")
-    Copley_B = Subway("Arlington", "Green Line -- B")
-    Hynes_B = Subway("Hynes Convention Center", "Green Line -- B")
-    Kenmore_B = Subway("Kenmore", "Green Line -- B")
+    Park_Street_B = Subway("Park Street", "Green Line -- any")
+    Boylston_B = Subway("Boylston", "Green Line -- any")
+    Arlington_B = Subway("Arlington", "Green Line -- any")
+    Copley_B = Subway("Copley", "Green Line -- any")
+    Hynes_B = Subway("Hynes Convention Center", "Green Line -- B, C, or D")
+    Kenmore_B = Subway("Kenmore", "Green Line -- B, C, or D")
     Blandford_B = Subway("Blandford Street", "Green Line -- B")
 
-    Park_Street_C = Subway("Park Street", "Green Line -- C")
-    Boylston_C = Subway("Boylston", "Green Line -- C")
-    Arlington_C = Subway("Arlington", "Green Line -- C")
-    Copley_C = Subway("Arlington", "Green Line -- C")
-    Hynes_C = Subway("Hynes Convention Center", "Green Line -- C")
-    Kenmore_C = Subway("Kenmore", "Green Line -- C")
+    Park_Street_C = Subway("Park Street", "Green Line -- any")
+    Boylston_C = Subway("Boylston", "Green Line -- any")
+    Arlington_C = Subway("Arlington", "Green Line -- any")
+    Copley_C = Subway("Arlington", "Green Line -- any")
+    Hynes_C = Subway("Hynes Convention Center", "Green Line -- B, C, or D")
+    Kenmore_C = Subway("Kenmore", "Green Line -- B, C, or D")
     Saint_Mary_C = Subway("Saint Mary's Street", "Green Line -- C")
 
     Union_Square_D = Subway("Union Square", "Green Line -- D")
@@ -71,13 +92,13 @@ def main():
     Science_Park_D = Subway("Science Park/West End", "Green Line -- D")
     North_Station_D = Subway("North Station", "Green Line -- D")
     Haymarket_D = Subway("Haymarket", "Green Line -- D")
-    Government_Center_D = Subway("Government Center", "Green Line -- D")
-    Park_Street_D = Subway("Park Street", "Green Line -- D")
-    Boylston_D = Subway("Boylston", "Green Line -- D")
-    Arlington_D = Subway("Arlington", "Green Line -- D")
-    Copley_D = Subway("Arlington", "Green Line -- D")
-    Hynes_D = Subway("Hynes Convention Center", "Green Line -- D")
-    Kenmore_D = Subway("Kenmore", "Green Line -- D")
+    Government_Center_D = Subway("Government Center", "Green Line -- any")
+    Park_Street_D = Subway("Park Street", "Green Line -- any")
+    Boylston_D = Subway("Boylston", "Green Line -- any")
+    Arlington_D = Subway("Arlington", "Green Line -- any")
+    Copley_D = Subway("Copley", "Green Line -- any")
+    Hynes_D = Subway("Hynes Convention Center", "Green Line -- B, C, or D")
+    Kenmore_D = Subway("Kenmore", "Green Line -- B, C, or D")
     Fenway_D = Subway("Fenway", "Green Line -- D")
 
     Medford_E = Subway("Medford/Tufts", "Green Line -- E")
@@ -89,16 +110,18 @@ def main():
     Science_Park_E = Subway("Science Park/West End", "Green Line -- E")
     North_Station_E = Subway("North Station", "Green Line -- E")
     Haymarket_E = Subway("Haymarket", "Green Line -- E")
-    Government_Center_E = Subway("Government Center", "Green Line -- E")
-    Park_Street_E = Subway("Park Street", "Green Line -- E")
-    Boylston_E = Subway("Boylston", "Green Line -- E")
-    Arlington_E = Subway("Arlington", "Green Line -- E")
-    Copley_E = Subway("Arlington", "Green Line -- E")
+    Government_Center_E = Subway("Government Center", "Green Line -- any")
+    Park_Street_E = Subway("Park Street", "Green Line -- any")
+    Boylston_E = Subway("Boylston", "Green Line -- any")
+    Arlington_E = Subway("Arlington", "Green Line -- any")
+    Copley_E = Subway("Copley", "Green Line -- any")
     Prudential_E = Subway("Prudential", "Green Line -- E")
 
     station_list = [Alewife, Davis, Porter, Harvard, Central, Kendall_MIT, Charles_MGH, Park_Street, Downtown_Crossing,
                     South_Station, Broadway, Andrew, JFK_UMass, Government_Center, Boylston, Savin_Hill, Fields_Corner, Shawmut,
-                    Ashmont, North_Quincy, Wollaston, Quincy_Center, Quincy_Adams, Braintree]
+                    Ashmont, North_Quincy, Wollaston, Quincy_Center, Quincy_Adams, Braintree, Arlington, Copley, Hynes_Convention,
+                    Kenmore, Blandford, Saint_Mary, Fenway, Prudential, Union_Square, Lechmere, Science_Park, North_Station,
+                    Haymarket, Tufts, Ball_Square, Magoun_Square, Gilman_Square, East_Som]
 
     station_graph = {Alewife : [Davis_Red],
                      Davis : [Porter_Red],
@@ -107,7 +130,7 @@ def main():
                      Central : [Kendall_Red],
                      Kendall_MIT : [Charles_Red],
                      Charles_MGH : [Park_Street_Red],
-                     Park_Street : [Downtown_Red, Boylston_B],
+                     Park_Street : [Downtown_Red, Boylston_B, Boylston_C, Boylston_D, Boylston_E],
                      Downtown_Crossing : [South_Station_Red],
                      South_Station : [Broadway_Red],
                      Broadway : [Andrew_Red],
@@ -122,11 +145,19 @@ def main():
                      Quincy_Center : [Quincy_Adams_Red],
                      Quincy_Adams : [Braintree_Red],
                      Braintree : [],
-                     Government_Center : [Park_Street_B],
-                     Boylston : []
+                     Government_Center : [Park_Street_B, Park_Street_C, Park_Street_D, Park_Street_E],
+                     Boylston : [Arlington_B, Arlington_C, Arlington_D, Arlington_E],
+                     Arlington : [Copley_B, Copley_C, Copley_D, Copley_E],
+                     Copley : [Hynes_B, Hynes_C, Hynes_D, Prudential_E],
+                     Prudential : [],
+                     Hynes_Convention : [Kenmore_B, Kenmore_C, Kenmore_D],
+                     Kenmore : [Blandford_B, Saint_Mary_C, Fenway_D],
+                     Blandford : [],
+                     Saint_Mary : [],
+                     Fenway : [],
                      }
     
-    ride_map(station_graph, station_list, Alewife, Ashmont, [])
+    ride_map(station_graph, station_list, Central, Prudential, [])
     
 
 def ride_map(stat_graph, stat_list, start, end, path):
@@ -137,11 +168,12 @@ def ride_map(stat_graph, stat_list, start, end, path):
 
     line_list = []
 
+    station_counts = defaultdict(int)
+
     if start.name == end.name:
         print("You are already at your intended destination.")
         return queue
     
-    i = 0
     while queue:
         path = queue.pop(0)
         node = path[-1]
@@ -151,11 +183,11 @@ def ride_map(stat_graph, stat_list, start, end, path):
             if type(node) != Station:
                 curr_route = node.route
                 curr_name = node.name
-                line_list.append((curr_name, curr_route, i))
+                line_list.append((curr_name, curr_route))
                 for station in stat_list:
                     if station.name == node.name:
                         node = station
-                i += 1
+
             neighbors = stat_graph[node]
             for neighbor in neighbors:
                 new_path = list(path)
@@ -163,24 +195,33 @@ def ride_map(stat_graph, stat_list, start, end, path):
                 queue.append(new_path)
 
                 if neighbor.name == end.name:
-
+                    line_list.append((neighbor.name, neighbor.route))
                     names_path = []
                     for stop in new_path:
                         names_path.append(stop.name)
-                    line_list.append((neighbor.name, neighbor.route, i))
+                        
                     route = line_list[0][1]
                     print(">>", route)
+                    
+                    station_counts = defaultdict(int)
 
-                    i = 1
+                    updated_line_list = []
+                    for line in line_list:
+                        if line[0] in names_path and station_counts[line[0]] == 0:
+                            updated_line_list.append(line)
+                            station_counts[line[0]] += 1
+
+                    j = 0
                     for station in new_path:
+                        if j < len(updated_line_list):
+                            if updated_line_list[j][0] in names_path:
+                                if updated_line_list[j][1] != route:
+                                    print(">>", updated_line_list[j][1])
+                                    route = updated_line_list[j][1]
                         print(station.name)
-                        if i < len(line_list):
-                            if line_list[i][0] in names_path:
-                                if line_list[i][1] != route:
-                                    print(">>", line_list[i][1])
-                                    route = line_list[i][1]
-                        i += 1
+                        j += 1
                     return new_path
+                
             explored.append(node)
     
     return None
